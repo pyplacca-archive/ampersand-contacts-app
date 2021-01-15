7eaa89c9d3920d348e5e79caf7d3d3cdc22f3837
pyPlacca <dhegreatprosperity10@gmail.com>
2021-01-15 05:27:45 +0000

    create custom app button (mostly for registration screens)

---
+++ src/components/Button.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Pressable, Text } from 'react-native';
+import { colors } from '../config';
+
+
+export default function Button ({children, onPress, style={}}) {
+	return (
+		<Pressable
+			style={{
+				borderRadius: 5,
+				backgroundColor: colors.primary,
+				padding: 17,
+				alignItems: 'center',
+				justifyContent: 'center',
+				...style
+			}}
+		>
+			<Text
+				style={{
+					color: '#fff',
+					textTransform: 'uppercase',
+					letterSpacing: 2.5
+				}}
+			>
+				{ children }
+			</Text>
+		</Pressable>
+	)
+}
