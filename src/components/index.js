1c1c3c487546459cd0aa0108560c82e46f779966
pyPlacca <dhegreatprosperity10@gmail.com>
2021-01-16 12:12:50 +0000

    add ProfileCard component to components module

--- src/components/index.js
+++ src/components/index.js
@@ -1,10 +1,12 @@
 import LinkButton from './LinkButton';
 import InputGroup from './InputGroup';
 import Button from './Button';
+import ProfileCard from './ProfileCard';
 
 
 export {
 	LinkButton,
 	InputGroup,
-	Button
+	Button,
+	ProfileCard
 }
