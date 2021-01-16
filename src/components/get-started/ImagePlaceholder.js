c378d8e548ffe626254c31a9f3b3947ecdb9d158
pyPlacca <dhegreatprosperity10@gmail.com>
2021-01-16 11:48:27 +0000

    make ImagePlacholder component pressable

--- src/components/get-started/ImagePlaceholder.js
+++ src/components/get-started/ImagePlaceholder.js
@@ -1,31 +1,42 @@
 import React from 'react';
-import { Image, View, Text } from 'react-native';
+import { Image, View, Text, Pressable } from 'react-native';
 import { AntDesign } from '@expo/vector-icons';
 import { colors } from '../../config';
 
 
-export default function ImagePlaceholder ({style}) {
+export default function ImagePlaceholder ({style, onPress}) {
 	return (
-		<View
-			style={{
-				alignItems: 'center',
-				justifyContent: 'center',
-				flex: 1,
-				backgroundColor: colors.grey,
-				...style
-			}}
-		>
-			<AntDesign name="user" size={40} color={colors.primary} />
-			<Text
+			<View
 				style={{
-					textTransform: 'uppercase',
-					fontWeight: 'bold',
-					color: colors.primary,
-					marginTop: 10
+					flex: 1
 				}}
 			>
-				Add profile photo
-			</Text>
-		</View>
+			<Pressable
+				onPress={onPress}
+				android_ripple={{
+					borderless: false,
+					color: '#f4f4f4'
+				}}
+				style={{
+					alignItems: 'center',
+					justifyContent: 'center',
+					flex: 1,
+					backgroundColor: colors.grey,
+					...style
+				}}
+			>
+				<AntDesign name="user" size={35} color={colors.primary}/>
+				<Text
+					style={{
+						textTransform: 'uppercase',
+						fontWeight: 'bold',
+						color: colors.primary,
+						marginTop: 10
+					}}
+				>
+					Add profile photo
+				</Text>
+			</Pressable>
+			</View>
 	)
 };
