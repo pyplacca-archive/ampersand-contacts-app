95f9fa438a1ab8488a5ec34e724ea886d12074af
pyPlacca <dhegreatprosperity10@gmail.com>
2021-01-16 12:56:20 +0000

    recolor text and increase font sizes

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { View, Text, Image } from 'react-native';
+import { colors } from '../config';
 
 
 export default function ProfileCard ({name, role, photo, style}) {
@@ -18,8 +19,8 @@ export default function ProfileCard ({name, role, photo, style}) {
 					height: 55,
 					resizeMode: 'cover',
 					borderRadius: 80,
-					borderWidth: 1,
-					borderColor: '#000'
+					// borderWidth: 1,
+					// borderColor: '#000'
 				}}
 			/>
 			<View
@@ -28,8 +29,23 @@ export default function ProfileCard ({name, role, photo, style}) {
 					marginLeft: 15,
 				}}
 			>
-				<Text>{name}</Text>
-				<Text>{role}</Text>
+				<Text
+					style={{
+						fontWeight: 'bold',
+						fontSize: 17,
+						marginBottom: 5
+					}}
+				>
+					{name}
+				</Text>
+				<Text
+					style={{
+						color: colors.textGrey,
+						fontSize: 17,
+					}}
+				>
+					{role}
+				</Text>
 			</View>
 		</View>
 	)
