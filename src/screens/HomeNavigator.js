94171f024c28b4b9ba83e9fb121a44aba18668b5
pyPlacca <dhegreatprosperity10@gmail.com>
2021-01-16 11:45:02 +0000

    add screens to Home navigator

--- src/screens/HomeNavigator.js
+++ src/screens/HomeNavigator.js
@@ -1,11 +1,27 @@
-import React from 'react';
-import StackNavigation from '../components/navigators';
+import React, { useContext } from 'react';
+import { StackNavigation } from '../components/navigators';
+import { HomeScreen, MyProfile } from './home';
+import HomeScreenHeader from '../components/home/Header';
 
 
 function HomeNavigator ({navigation}) {
 	return (
 		<StackNavigation
 			screens={[
+				{
+					name: 'home',
+					component: HomeScreen,
+					options: {
+						header: props => <HomeScreenHeader {...props}/>,
+					}
+				},
+				{
+					name: 'my-profile',
+					component: MyProfile,
+					options: {
+						title: 'My Profile'
+					}
+				}
 			]}
 		/>
 	)
