4087c7679ac35eb13ff8d9192e6fba09284a9cbe
pyPlacca <dhegreatprosperity10@gmail.com>
2021-01-16 11:49:32 +0000

    create a custom header for the home screen

---
+++ src/components/home/Header.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Pressable, View, StatusBar } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+import { HeaderLogo } from '../header';
+import { colors } from '../../config';
+
+
+export default function HomeScreenHeader ({navigation}) {
+	const [paddingTop, iconSize, logoHeight] = [
+		StatusBar.currentHeight + 20, 24, 35
+	];
+	return (
+		<View
+			style={{
+				position: 'relative',
+				flexDirection: 'row',
+				alignItems: 'center',
+				justifyContent: 'center',
+				paddingTop,
+				backgroundColor: colors.primary
+			}}
+		>
+			<HeaderLogo
+				style={{
+					margin: 0,
+					height: logoHeight
+				}}
+			/>
+			<Pressable
+				onPress={() => navigation.navigate('my-profile')}
+				style={{
+					position: 'absolute',
+					right: 30,
+					top: paddingTop - (iconSize / 2) + (logoHeight / 2)
+				}}
+			>
+				<AntDesign name="user" size={iconSize} color="#fff"/>
+			</Pressable>
+		</View>
+	)
+};
