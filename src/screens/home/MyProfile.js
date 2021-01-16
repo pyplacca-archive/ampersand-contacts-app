5a77d26022d5f61f7a09019fa958c84b34563574
pyPlacca <dhegreatprosperity10@gmail.com>
2021-01-16 14:42:59 +0000

    build profile screen

--- src/screens/home/MyProfile.js
+++ src/screens/home/MyProfile.js
@@ -1,6 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { View } from 'react-native';
+import { ProfileDetails } from '../../components';
+import { AppContext } from '../../store'
 
 
 export default function MyProfile () {
-	return null
+
+	const {state: {user}} = useContext(AppContext);
+
+	return (
+		<View
+			style={{
+				flex: 1,
+				padding: 20,
+				paddingVertical: 30,
+			}}
+		>
+			<StatusBar style='light'/>
+			<ProfileDetails {...user}/>
+		</View>
+	)
 };
