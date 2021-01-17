ab3928b4210d6aad83b084d74257ca7bdc6f212b
pyPlacca <dhegreatprosperity10@gmail.com>
2021-01-17 21:39:17 +0000

    Create MemberProfile.js

---
+++ src/screens/home/MemberProfile.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { View } from 'react-native';
+import { ProfileDetails } from '../../components';
+
+
+export default function MemberProfile ({route}) {
+	return (
+		<View
+			style={{
+				flex: 1,
+				padding: 20,
+				paddingVertical: 30,
+			}}
+		>
+			<StatusBar style='light' hidden={false}/>
+			<ProfileDetails
+				name='Drew Barnes'
+				role='Tech Lead & Co-Founder'
+				social={{
+					twitter: '@drew_bms',
+					linkedin: '@lht'
+				}}
+				telephone='+233 (245) 234 123'
+				email='drewbarnes@ampersandllc.co'
+				photo={{uri: 'https://ampersandllc.co/wp-content/uploads/2019/11/drew.jpg'}}
+			/>
+		</View>
+	)
+};
