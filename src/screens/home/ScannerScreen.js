409080e9e8706b333e92ead79b8ead5d2f716791
pyPlacca <dhegreatprosperity10@gmail.com>
2021-01-17 21:38:23 +0000

    Add QR code scanning functionality

---
+++ src/screens/home/ScannerScreen.js
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, Image, Pressable } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import { Ionicons } from '@expo/vector-icons';
+import { BottomSection } from '../../components/home';
+import Scan from '../../icons/scan.svg';
+
+
+export default function QRScanner ({navigation}) {
+	const [hasPermission, setHasPermission] = useState(null);
+
+  useEffect(() => {
+  	// request for permission to access device camera to scan BarCode
+    (async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  const _onBarCodeScanned = (result) => {
+  	navigation.navigate('member-profile')
+		// navigation.navigate('home', {scan: result})
+  }
+
+  const exitScanner = () => {
+  	navigation.navigate('home', {scan: null})
+  }
+
+	return (
+		<View style={{ flex: 1 }}>
+			<StatusBar hidden/>
+			<View
+				style={{
+					flexGrow: 1,
+					justifyContent: 'center',
+					alignItems: 'center',
+					backgroundColor: '#000',
+					color: '#fff',
+				}}
+			>
+				<View
+					style={{
+						flexDirection: 'row',
+						alignItems: 'center',
+						justifyContent: 'space-between',
+						paddingHorizontal: 30,
+						paddingVertical: 20,
+						width: '100%',
+						position: 'absolute',
+						top: 20,
+						left: 0,
+						zIndex: 10
+					}}
+				>
+					<Ionicons name="flash-sharp" size={20} color="grey"/>
+					<Pressable onPress={exitScanner}>
+						<Ionicons name="close-outline" size={24} color="white"/>
+					</Pressable>
+				</View>
+				{
+					hasPermission === null ? (
+						<Text>Requesting for camera permission...</Text>
+					) : hasPermission === false ? (
+						<Text>No access to camera</Text>
+					) : (
+						<React.Fragment>
+							<BarCodeScanner
+								onBarCodeScanned={_onBarCodeScanned}
+								style={{
+									width: '100%',
+									flexGrow: 1
+								}}
+							/>
+							<View
+								style={{
+									position: 'absolute',
+									alignItems: 'center',
+								}}
+							>
+								<Scan width={250} height={250}/>
+								<Text
+									style={{
+										color: '#fff',
+										fontSize: 18,
+										marginTop: 50
+									}}
+								>
+									Place QR Code Within Frame
+								</Text>
+							</View>
+						</React.Fragment>
+					)
+				}
+			</View>
+			<BottomSection
+				text='Want to share your contact'
+				btnText='Send QR'
+				onBtnPress={exitScanner}
+			/>
+		</View>
+	)
+};
