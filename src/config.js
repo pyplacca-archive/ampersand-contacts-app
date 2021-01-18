446355a0c0b78798d9c4afb40a3fd91205d36358
pyPlacca <dhegreatprosperity10@gmail.com>
2021-01-18 13:56:51 +0000

    add configuration object for horizontalCardStyle interpolation

--- src/config.js
+++ src/config.js
@@ -1,3 +1,6 @@
+import { CardStyleInterpolators } from '@react-navigation/stack';
+
+
 export const colors = {
 	primary: '#fe2b4c',
 	secondary: '#434c4e',
@@ -5,3 +8,22 @@ export const colors = {
 	textPrimary: '#434c4e',
 	textGrey: '#acacac'
 }
+
+export const horizontalCardStyle = {
+	gestureEnabled: true,
+	gestureDirection: 'horizontal',
+	gestureResponseDistance: {
+		horizontal: 250
+	},
+	cardStyle: {
+		shadowColor: '#000',
+		shadowOpacity: .3,
+		shadowRadius: 7,
+		shadowOffset: {
+      width: -3,
+      height: 0,
+    },
+    elevation: 5,
+	},
+	cardStyleInterpolator: CardStyleInterpolators.forHorizontalIOS,
+};
