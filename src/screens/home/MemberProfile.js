e2bc806b871930cba06fecf43d4d2c530c71da5a
pyPlacca <dhegreatprosperity10@gmail.com>
2021-01-19 21:52:03 +0000

    display appropriate profile or message based on qr code data

--- src/screens/home/MemberProfile.js
+++ src/screens/home/MemberProfile.js
@@ -4,7 +4,7 @@ import { View } from 'react-native';
 import { ProfileDetails } from '../../components';
 
 
-export default function MemberProfile ({route}) {
+export default function MemberProfile ({route: {params}}) {
 	return (
 		<View
 			style={{
@@ -15,15 +15,16 @@ export default function MemberProfile ({route}) {
 		>
 			<StatusBar style='light' hidden={false}/>
 			<ProfileDetails
-				name='Drew Barnes'
-				role='Tech Lead & Co-Founder'
-				social={{
-					twitter: '@drew_bms',
-					linkedin: '@lht'
-				}}
-				telephone='+233 (245) 234 123'
-				email='drewbarnes@ampersandllc.co'
-				photo={{uri: 'https://ampersandllc.co/wp-content/uploads/2019/11/drew.jpg'}}
+				// name='Drew Barnes'
+				// role='Tech Lead & Co-Founder'
+				// social={{
+				// 	twitter: '@drew_bms',
+				// 	linkedin: '@lht'
+				// }}
+				// telephone='+233 (245) 234 123'
+				// email='drewbarnes@ampersandllc.co'
+				// photo={{uri: 'https://ampersandllc.co/wp-content/uploads/2019/11/drew.jpg'}}
+				{...params.details}
 			/>
 		</View>
 	)
