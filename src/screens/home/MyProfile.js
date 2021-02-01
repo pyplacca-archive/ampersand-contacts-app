4755704e37df1ec2215757a0f7afbd86a279fa2a
pyPlacca <dhegreatprosperity10@gmail.com>
2021-02-01 19:23:36 +0000

    add a sign out button on profile screen

--- src/screens/home/MyProfile.js
+++ src/screens/home/MyProfile.js
@@ -2,13 +2,19 @@ import React, { useContext } from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { View } from 'react-native';
 import { connect } from 'react-redux';
-import { ProfileDetails } from '../../components';
+import { auth } from '../../firebase';
+import { ProfileDetails, Button } from '../../components';
 // import { AppContext } from '../../store'
 
 
-function MyProfile ({user}) {
+function MyProfile ({user, dispatch}) {
 
 	// const {state: {user}} = useContext(AppContext);
+
+	const signOut = () => {
+		auth.signOut()
+		.then(() => dispatch({type: 'sign_out'}))
+	}
 
 	return (
 		<View
@@ -20,6 +26,14 @@ function MyProfile ({user}) {
 		>
 			<StatusBar style='light'/>
 			<ProfileDetails {...user}/>
+			<Button
+				style={{
+					marginTop: 30
+				}}
+				onPress={signOut}
+			>
+				Sign Out
+			</Button>
 		</View>
 	)
 };
