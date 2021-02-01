b97941741f8b690aca530491fdcbdc317021db86
pyPlacca <dhegreatprosperity10@gmail.com>
2021-02-01 19:30:54 +0000

    connect redux store to App and persist it

--- App.js
+++ App.js
@@ -2,10 +2,18 @@ import React from 'react';
 import { useFonts } from '@use-expo/font';
 import AppLoading from 'expo-app-loading';
 import AppNavigator from './src/AppNavigator';
-import AppProvider from './src/store';
+// import AppProvider from './src/store';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './src/store';
+
+import { LogBox } from 'react-native';
 
 
 export default function App () {
+  // ignore timer warnings raised due firebase's MAX_RETRY issue
+  LogBox.ignoreLogs(['Setting a timer']);
+
   const [loaded] = useFonts({
     // WorkSans: require('./assets/fonts/Work-Sans.ttf'),
   })
@@ -13,8 +21,10 @@ export default function App () {
   return !loaded ? (
     <AppLoading/>
   ) : (
-    <AppProvider>
-      <AppNavigator/>
-    </AppProvider>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <AppNavigator/>
+      </PersistGate>
+    </Provider>
   )
 }
