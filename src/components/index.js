c4511aa6dcdb855eafb9a330c142a614fc06c814
pyPlacca <dhegreatprosperity10@gmail.com>
2021-02-01 10:55:18 +0000

    add ErrorLabel component to components module

--- src/components/index.js
+++ src/components/index.js
@@ -1,8 +1,24 @@
+import { Text } from 'react-native';
+import { colors } from '../config';
+// custom components
 import LinkButton from './LinkButton';
 import InputGroup from './InputGroup';
 import Button from './Button';
 import ProfileCard from './ProfileCard';
 import ProfileDetails from './Details';
+
+function ErrorLabel (props) {
+	return (
+		<Text
+			style={{
+				color: colors.primary,
+				marginBottom: 20
+			}}
+		>
+			{props.children}
+		</Text>
+	)
+};
 
 
 export {
@@ -10,5 +26,6 @@ export {
 	InputGroup,
 	Button,
 	ProfileCard,
-	ProfileDetails
+	ProfileDetails,
+	ErrorLabel
 }
