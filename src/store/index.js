c4d97812a068292c4bf45ac745e452ca84a2218c
pyPlacca <dhegreatprosperity10@gmail.com>
2021-02-01 10:58:21 +0000

    switch from Context API to Redux for state management
    The switch was necessary because of the need for store/state persistence (for login sessions), and redux-persist offer that functionality out of the box.

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,10 @@
-import React from 'react';
-import { createContext, useReducer } from 'react';
-import appReducer from './reducer'
+// import React from 'react';
+// import { createContext, useReducer } from 'react';
+import appReducer from './reducer';
+
+import { createStore } from 'redux';
+import { persistReducer, persistStore } from 'redux-persist';
+import { AsyncStorage as storage } from 'react-native';
 
 
 /*
@@ -8,32 +12,50 @@ import appReducer from './reducer'
 */
 const initialState = {
 	signedIn: false,
-	user: {
-		name: 'Lady-Omega Hammond',
-		role: 'Team Lead & Co-Founder',
-		photo: {
-			uri: 'https://www.ampersandllc.co/wp-content/uploads/2019/10/Miss-Hammond.jpg'
-		},
-		telephone: '+233 12 345 6789',
-		email: 'ladyomega@ampersandllc.co',
-		location: 'Accra, Ghana',
-		social: {
-			twitter: '@Lonnah',
-			linkedin: 'lady omega',
-		}
+	user: null,
+	// user: {
+	// 	name: 'Lady-Omega Hammond',
+	// 	role: 'Team Lead & Co-Founder',
+	// 	photo: {
+	// 		uri: 'https://www.ampersandllc.co/wp-content/uploads/2019/10/Miss-Hammond.jpg'
+	// 	},
+	// 	telephone: '+233 12 345 6789',
+	// 	email: 'ladyomega@ampersandllc.co',
+	// 	location: 'Accra, Ghana',
+	// 	social: {
+	// 		twitter: '@Lonnah',
+	// 		linkedin: 'lady omega',
+	// 	}
+	// },
+	registered_users: {}
+}
+
+// export const AppContext = createContext();
+
+// function AppProvider (props) {
+// 	const [state, dispatch] = useReducer(appReducer, initialState);
+
+// 	return (
+// 		<AppContext.Provider value={{state, dispatch}}>
+// 			{ props.children }
+// 		</AppContext.Provider>
+// 	)
+// }
+
+// export default AppProvider;
+
+const store = createStore(persistReducer(
+	// configuration for store persistence
+	{
+		key: 'root',
+		storage
 	},
+	appReducer
+));
+
+const persistor = persistStore(store);
+
+export {
+	store,
+	persistor
 }
-
-export const AppContext = createContext();
-
-function AppProvider (props) {
-	const [state, dispatch] = useReducer(appReducer, initialState);
-
-	return (
-		<AppContext.Provider value={{state, dispatch}}>
-			{ props.children }
-		</AppContext.Provider>
-	)
-}
-
-export default AppProvider;
