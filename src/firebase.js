e433a89178293befefe8971c2cca3d276f153290
pyPlacca <dhegreatprosperity10@gmail.com>
2021-02-01 10:55:45 +0000

    setup firebase authentication and data storage

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import * as firebase from 'firebase';
+import 'firebase/auth';
+import "firebase/firestore";
+
+
+firebase.initializeApp({
+  apiKey: "AIzaSyDv8Wr5cp6z_FYcCBYypL3gvioc-bO0bs8",
+  authDomain: "ct-g14-m.firebaseapp.com",
+  databaseURL: "https://ct-g14-m.firebaseio.com",
+  projectId: "ct-g14-m",
+  storageBucket: "ct-g14-m.appspot.com",
+  messagingSenderId: "671773754814",
+  appId: "1:671773754814:web:c34ed82823ebfd51bce5ef",
+  measurementId: "G-738ZP3G9MS"
+})
+
+export default firebase;
